183e83bc872d00acbf1398f86f34aabd2fc23d06
Dandridge Cole <dandridgecole@yahoo.com>
2019-06-05 18:57:59 -0400

    Done several methods along with their test cases

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,69 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+
 public class Student {
+    String firstName;
+    String lastName;
+    ArrayList<Double> examScores;
+
+
+    public Student(String firstName, String lastName, ArrayList<Double> testScores) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.examScores = testScores;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getNumberOfExamsTaken(){
+        return examScores.size();
+    }
+
+    public String getExamScores(){
+        StringBuilder output = new StringBuilder("Exam Scores:");
+        for (int i = 0; i < examScores.size(); i++) {
+            output.append(String.format("\n\tExam %d -> %1.0f",i+1,examScores.get(i)));
+        }
+        return output.toString();
+    }
+
+    public void addExamScore(Double newScore){
+        examScores.add(newScore);
+    }
+
+    public void setExamScore(int examNumber, Double newScore){
+        examScores.set(examNumber-1,newScore);
+    }
+
+    public Double getAverageExamScore(){
+        Double sum = 0.0;
+        for (Double score:examScores) {
+            sum+=score;
+        }
+        return sum/examScores.size();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder("Student Name: "+this.getFirstName()+" "+this.getLastName());
+        builder.append(String.format("\n> Average Score: %1.0f",this.getAverageExamScore()));
+        builder.append("\n> ");
+        builder.append(getExamScores());
+        return builder.toString();
+    }
 }
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,5 +1,71 @@
 package io.zipcoder;
 
+import org.junit.Test;
+import java.util.ArrayList;
+import java.util.Arrays;
+import org.junit.Assert;
+
 public class StudentTest {
 
+    @Test
+    public void getExamScoresTest(){
+        String expected="Exam Scores:" +
+                "\n\tExam 1 -> 100" +
+                "\n\tExam 2 -> 95" +
+                "\n\tExam 3 -> 123" +
+                "\n\tExam 4 -> 96";
+        ArrayList<Double> scores = new ArrayList<Double>(Arrays.asList(100.0,95.0,123.0,96.0));
+         Student student = new Student("Jon","Smith",scores);
+         String actual = student.getExamScores();
+         Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void addExamScoreTest(){
+        ArrayList<Double> scores = new ArrayList<>();
+        scores.clear();
+        Student student = new Student(null,null, scores);
+        student.addExamScore(100.0);
+        String expected="Exam Scores:" +
+                "\n\tExam 1 -> 100";
+        String actual = student.getExamScores();
+        Assert.assertEquals(expected,actual);
+
+    }
+
+    @Test
+    public void setExamScoreTest(){
+        ArrayList<Double> scores = new ArrayList<Double>(Arrays.asList(100.0));
+        Student student = new Student(null,null, scores);
+        student.setExamScore(1,150.0);
+        String expected="Exam Scores:" +
+                "\n\tExam 1 -> 150";
+        String actual = student.getExamScores();
+        Assert.assertEquals(expected,actual);
+
+    }
+
+    @Test
+    public void getAvgScoreTest(){
+        ArrayList<Double> scores = new ArrayList<Double>(Arrays.asList(100.0, 150.0, 250.0, 0.0));
+        Student student = new Student("John","Doe", scores);
+        Double actual = student.getAverageExamScore();
+        Double expected=125.0;
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void toStringTest(){
+        ArrayList<Double> scores = new ArrayList<Double>(Arrays.asList(100.0, 150.0, 250.0, 0.0));
+        Student student = new Student("John","Doe", scores);
+        String actual = student.toString();
+        String expected="Student Name: John Doe" +
+                "\n> Average Score: 125" +
+                "\n> Exam Scores:" +
+                "\n\tExam 1 -> 100" +
+                "\n\tExam 2 -> 150" +
+                "\n\tExam 3 -> 250" +
+                "\n\tExam 4 -> 0";
+        Assert.assertEquals(expected,actual);
+    }
 }
